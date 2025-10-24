0b0cc204f994872471090a883647e4877b9481a4
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-10-25 01:53:18 +0530

    route and controller

--- src/app.js
+++ src/app.js
@@ -13,5 +13,14 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" })) // for URL data
 app.use(express.static("public"))  // for static files data
 app.use(cookieParser()) // for cookie data
 
+// routes import
 
+import userRouter from "./routes/user.routes.js"
+
+
+// routes declaration
+
+app.use("/api/v1/users",userRouter)
+
+// http:localhost:8000/api/v1/users/register 
 export { app }
