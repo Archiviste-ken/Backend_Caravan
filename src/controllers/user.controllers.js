2b73b1c336e8924332df27752d57e152affc8848
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-10-25 14:16:18 +0530

    chandu beta masti nai

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,8 +2,22 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
+    // logic to register user
+    // get user data from frontend
+    // validation - not empty
+    // check if user already exists
+    // check for images, check for avatar
+    // upload them to cloudinary ,avatar
+    //create user object - creation entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return response to frontend
+
+    //  const {fullname,email,username,password}= req.body
+    //  console.log("email: ",email);
+     
+    res.status(500).json({
+        message: "chandu is gay"
     })
 })
 
